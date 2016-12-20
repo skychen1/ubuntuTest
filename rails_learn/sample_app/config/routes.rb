557807f20646cebb6bf781010d498c49a74111e6
skychen1 <1351483724@qq.com>
2016-12-20 16:52:40 +0800

    添加路由

--- rails_learn/sample_app/config/routes.rb
+++ rails_learn/sample_app/config/routes.rb
@@ -1,9 +1,16 @@
 SampleApp::Application.routes.draw do
+  #get "users/new"
+get 'signup' =>'users#new'
+
   root :to => 'static_pages#home'
 
-  get "static_pages/help"
+  
+  #get "static_pages/help"
+  get 'help' =>'static_pages#help'
 
   get "static_pages/about"
+
+  get "static_pages/contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
